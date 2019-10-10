6deeae255828203113ee8ddac6240c3abfd9a849
Deacs <chilliweb@gmail.com>
2019-10-10 16:39:03 +0100

    Gravatar package installed

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -206,6 +207,7 @@ return [
         'Event' => Illuminate\Support\Facades\Event::class,
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
--- resources/views/dashboard/user_details.blade.php
+++ resources/views/dashboard/user_details.blade.php
@@ -6,20 +6,35 @@
         <label for="userName">Name</label>
         <input type="text" class="form-control" id="userName" name="userName" aria-describedby="nameHelp" placeholder="Enter name" value="{{ $user->name }}">
         <small id="nameHelp" class="form-text text-muted">As it will appear on your profile.</small>
+        @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
     <div class="form-group">
       <label for="userEmail">Email address</label>
       <input type="email" class="form-control" id="userEmail" name="userEmail" aria-describedby="emailHelp" placeholder="Enter email" value="{{ $user->email }}">
       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
     </div>
     <div class="form-group">
-    <label for="userGender">Gender</label>
+        <label for="userGender">Gender</label>
         <select class="form-control" id="userGenderId" name="userGenderId">
             <option>Please Select</option>
             <option value="1" @if ($user->gender_id === 1) selected="selected" @endif>Female</option>
             <option value="2" @if ($user->gender_id === 2) selected="selected" @endif>Male</option>
             <option value="3" @if ($user->gender_id === 3) selected="selected" @endif>Non-Binary</option>
         </select>
+        @error('gender_id')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
     <button type="submit" class="btn btn-primary">Update Details</button>
 </form>
